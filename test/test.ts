fc462bbf581b750b5f5984a9206654f2dfcd5ccc
RIJAB BUTT <rijabbutt23@gmail.com>
2024-05-12 15:18:37 +0500

    import export examples

---
+++ test/test.ts
@@ -0,0 +1,12 @@
+let a = 5
+const abc = "abc"
+let array = [1, 2, 3]
+const obj = {
+    name:"Rijab"
+}
+function sum() {
+    console.log("This is sum function");
+    
+}
+export default a;
+export {abc, array, obj, sum };
