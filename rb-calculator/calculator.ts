49bdfcce80b05630845b11d2f78bdfe58662d7f3
RIJAB BUTT <rijabbutt23@gmail.com>
2024-05-14 20:11:17 +0500

    node calculator with input examples export import

---
+++ rb-calculator/calculator.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env mode
+import inquirer from "inquirer";
+import { questions, sum, subtract, multiply, divide } from "./input.js";
+function check() {
+	if (questions.operator == "+") {
+		sum(questions.num1, questions.num2);
+	} else if (questions.operator == "-") {
+		subtract(questions.num1, questions.num2);
+	} else if (questions.operator == "*") {
+		multiply(questions.num1, questions.num2);
+	} else if (questions.operator == "/") {
+		divide(questions.num1, questions.num2);
+	} else {
+		console.log("Invalid operator");
+	}
+}
+check();
+console.log("questions", questions);
---
+++ rb-calculator/input.js
@@ -0,0 +1,50 @@
+import inquirer from "inquirer";
+// inquirer
+// 	.prompt([
+// 		/* Pass your questions in here */
+// 	])
+// 	.then((answers) => {
+// 		// Use user feedback for... whatever!!
+// 	})
+// 	.catch((error) => {
+// 		if (error.isTtyError) {
+// 			// Prompt couldn't be rendered in the current environment
+// 		} else {
+// 			// Something else went wrong
+// 		}
+// 	});
+const questions = await inquirer.prompt([
+    {
+        type: "number",
+        name: "num1",
+        message: "Enter your 1st number",
+    },
+    {
+        type: "number",
+        name: "num2",
+        message: "Enter your 2st number",
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Select your operator",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+function sum(num1, num2) {
+    const result = num1 + num2;
+    return console.log(`Sum of ${num1} and ${num2} = ${result}`);
+}
+function subtract(num1, num2) {
+    const result = num1 - num2;
+    return console.log(`Subtract of ${num1} and ${num2} = ${result}`);
+}
+function multiply(num1, num2) {
+    const result = num1 * num2;
+    return console.log(`Multiply of ${num1} and ${num2} = ${result}`);
+}
+function divide(num1, num2) {
+    const result = num1 / num2;
+    return console.log(`Division of ${num1} and ${num2} = ${result}`);
+}
+export { questions, sum, subtract, multiply, divide };
