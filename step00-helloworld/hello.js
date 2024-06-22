929bc1ec61a4cfe85bfae1aa9c2ef9f5441bb49e
RIJAB BUTT <rijabbutt23@gmail.com>
2024-06-22 22:16:44 +0500

    ..

--- step00-helloworld/hello.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-//  Set-ExecutionPolicy RemoteSigned
-//  npm init -y
-//  npm i @types/node -D
-// Create .gitignore file
-console.log("Hi to my node projects!");
--- step00-helloworld/hello.ts
+++
@@ -1,5 +0,0 @@
-//  Set-ExecutionPolicy RemoteSigned
-//  npm init -y
-//  npm i @types/node -D
-// Create .gitignore file
-console.log("Hi to my node projects!");
